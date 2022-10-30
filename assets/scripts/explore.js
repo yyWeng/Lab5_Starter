20adb6e21337465b70f9189a031f82cd1bf19d3e
Yiyu <1843044148W@gmail.com>
2022-10-30 14:40:50 -0700

    finish

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,41 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+  const textinput=document.getElementById('text-to-speak');
+  const vselector=document.getElementById('voice-select');
+  const btn=document.getElementsByTagName('button')[0];
+ 
+  let voices= [];
+  speechSynthesis.onvoiceschanged=function(){
+    let utterance = new SpeechSynthesisUtterance();
+    voices=speechSynthesis.getVoices();
+
+    for(let i=0;i<voices.length;i++){
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      option.value=voices[i].name;
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      vselector.appendChild(option);
+    }
+    vselector.addEventListener('change',(event)=>{
+      for(let j=0;j<voices.length;j++){
+        if(voices[j].name==event.target.value){
+          utterance.voice=voices[j];
+        }
+      }
+    })
+    textinput.addEventListener('input',(event)=>{
+      utterance.text=event.target.value;
+
+    })
+    btn.addEventListener('click',()=>{
+      speechSynthesis.speak(utterance); 
+    })
+    
+    
+  }
+
+  
 }
